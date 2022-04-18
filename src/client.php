4a897dff18b00a0dcbc55567ea65a0cd1f332b6d
webolc <516705893@qq.com>
2022-04-18 17:45:08 +0800

    'init'

--- src/client.php
+++ src/client.php
@@ -1,1 +1,36 @@
 <?php
+use chain33\phpsdk\ChainClient;
+
+class client{
+    /**
+     * 客户端
+     * @var unknown
+     */
+    private $client;
+    
+    /**
+     * 初始化
+     */
+    public function __construct(){
+        $this->client = new ChainClient();
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /**
+     * 获取chain33SDK接口
+     * @return unknown
+     */
+    public function chain33(){
+        return $this->client;
+    }
+}
+
+
+
