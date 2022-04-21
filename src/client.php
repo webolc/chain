493f78cf083f6696584ea5fc69c8c184de4bc51f
webolc <516705893@qq.com>
2022-04-21 16:49:10 +0800

    init

--- src/client.php
+++ src/client.php
@@ -1,18 +1,33 @@
 <?php
-use chain33\phpsdk\ChainClient;
+namespace webolc\chain;
 
 class client{
+    
     /**
-     * 客户端
-     * @var unknown
+     * 配置数据
+     * @var array
      */
-    private $client;
+    protected $config = [
+        'host' => '127.0.0.1',
+        'port' => '8801',
+        'timeout' => 60
+    ];
     
     /**
      * 初始化
      */
-    public function __construct(){
-        $this->client = new ChainClient();
+    public function __construct($config=[]){
+        if ($config) $this->config = array_merge($this->config,$config);
+    }
+    
+    /**
+     * 获取远程节点列表
+     * @return array
+     */
+    public function getPeerInfo()
+    {
+        $method = 'Chain33.GetPeerInfo';
+        return $this->sendRequest($method);
     }
     
     
@@ -23,12 +38,34 @@ class client{
     
     
     
+    
+    
     /**
-     * 获取chain33SDK接口
-     * @return unknown
+     * @param string $method
+     * @param array $params
+     * @return array
      */
-    public function chain33(){
-        return $this->client;
+    private function sendRequest(string $method,$params = [])
+    {
+        $id = time();
+        try {
+            $curl = new Curl($this->config);
+            $data = [
+                'id' => $id,
+                "jsonrpc" => "2.0",
+                'method' => $method,
+                'params' => empty($params) ? [] : [$params]
+            ];
+            
+            $curl->setData(json_encode($data));
+            $curl->setHeader(['Content-Type:application/json']);
+            $res = $curl->post();
+            $res = $res ? $res : ['id' => $id,'result' => [],'error' => 'request fail'];
+        } catch (\Exception $e) {
+            $res = ['id' => $id,'result' => [],'error' => $e->getMessage()];
+        }
+        unset($res['id']);
+        return $res;
     }
 }
 
